130c17de8bebd1b7e65e3ac2e11f369f27e31b4d
lukaskreibig <lukatze@posteo.de>
2021-06-15 12:31:53 +0200

    add axios and get API

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 function Leaderboard() {
-  return <div className="leaderboard"></div>;
+  return (
+    <>
+      <div className="lb-button">
+        <button variant="contained" color="primary">
+          Today
+        </button>
+        <button variant="contained" color="primary">
+          This Week
+        </button>
+        <button variant="contained" color="primary">
+          This Month
+        </button>
+      </div>
+      <div className="lb-best"></div>
+    </>
+  );
 }
 
 export default Leaderboard;
