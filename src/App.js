e81f7e5f9190923135f431d2d195676dba4527fa
lukaskreibig <lukatze@posteo.de>
2021-06-15 10:53:27 +0200

    new webpack settings, add style loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import "./styles.css";
 
 export default function App() {
-	return (
-		<div>
-			<Button variant="contained" color="primary">
-				Why do you hate me so much?
-			</Button>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Why do you hate me so much?</h1>
+    </div>
+  );
 }
