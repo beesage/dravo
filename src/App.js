954fae9f966d84f332247aba66512a22b8a9262a
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-15 16:17:37 +0200

    Add setting icon to header. Icon show only on UserProfile component. Add loading to axios

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import "./styles.css";
 
 import Header from "./components/Header";
 import Leaderboard from "./components/Leaderboard";
+import UserProfile from "./components/UserProfile";
 
 export default function App() {
 	const [user, setUser] = useState([]);
+	const [loading, setIsLoading] = useState(false);
 
 	const userAPI = () => {
+		setIsLoading(true);
 		axios
 			.get("http://202.61.225.240:3000/beewhoyouwant")
 			.then((res) => setUser(res.data));
+		setIsLoading(false);
 	};
 
 	useEffect(userAPI, []);
@@ -27,6 +31,10 @@ export default function App() {
 				<Header />
 				<Switch>
 					<Route path="/leaderboard" component={Leaderboard} />
+					<Route
+						path="/profile"
+						render={() => <UserProfile user={user} loading={loading} />}
+					/>
 				</Switch>
 			</Router>
 		</div>
