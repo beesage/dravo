3ab14693aec954313c73bd5c8f835f7749bf27d5
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-16 09:28:13 +0200

    Fix spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+
 import "./UserProfile-Header.css";
 
 import AppBar from "@material-ui/core/AppBar";
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaMedal } from "react-icons/fa";
 
 import "./UserProfile-Header.css";
 
@@ -6,17 +7,20 @@ export default function UserProfile({ user, loading }) {
 	return (
 		<div className="user-container">
 			<div className="user-profile">
-				<div className="user-info">
-					{user.length > 0 ? (
+				{user.length > 0 ? (
+					<>
 						<>
+							<FaMedal className="medal" />
+						</>
+						<div className="user-info">
 							<img className="profile-pic" src={user[0].profilepic} />
 							<p className="username">{user[0].beekeeper}</p>
 							<p className="user-location">{user[0].location}</p>
-						</>
-					) : (
-						loading
-					)}
-				</div>
+						</div>
+					</>
+				) : (
+					loading
+				)}
 			</div>
 		</div>
 	);
