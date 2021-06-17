f682632ad4c1505a13fe962c02bb0fbac7bfc95a
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-17 15:45:57 +0200

    Clean code

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -10,27 +10,16 @@ import HexData from "./HexData";
 export default function UserProfile({ user, loading }) {
 	return (
 		<div className="user-container">
-			<div className="user-profile">
-				{user.length > 0 ? (
-					<>
-						<>
-							<FaMedal className="medal" />
-						</>
-						<div className="user-info">
-							<>
-								<img className="profile-pic" src={user[0].profilepic} />
-								<p className="username">{user[0].beekeeper}</p>
-								<p className="user-location">{user[0].location}</p>
-							</>
-						</div>
-					</>
-				) : (
-					loading
-				)}
-			</div>
-
 			{user.length > 0 ? (
 				<>
+					<div className="user-profile">
+						<FaMedal className="medal" />
+						<div className="user-info">
+							<img className="profile-pic" src={user[0].profilepic} />
+							<p className="username">{user[0].beekeeper}</p>
+							<p className="user-location">{user[0].location}</p>
+						</div>
+					</div>
 					<div className="all-us-hex">
 						{Icons.map((icons, index) => (
 							<Hex icons={icons} key={index} />
