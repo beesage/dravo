0b67b949a817ffb749c82eab2762359a7f22143d
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-23 11:32:09 +0200

    Delete e-mail from @author

--- src/App.js
+++ src/App.js
@@ -15,15 +15,18 @@ export default function App() {
 	const [user, setUser] = useState([]);
 	const [loading, setLoading] = useState(false);
 
-	// @author Alessandra Pettinato <alessandrapettinato@hotmail.it>
+	// @author Alessandra Pettinato
 	// Fetch data from the specified API
 	//
+	// @function useEffect - React Hook function to perform side effects operations in function components
 	// @function userAPI
 	// @async
 	// @function setLoading
-	// @param {boolean} true - The value of setLoading is set to true while waiting for the function to execute
+	// @param {boolean} true - The value of setLoading is set to true while waiting for userAPI to execute
 	// @await
-	// @param {string} "http://202.61.225.240:3000/beewhoyouwant" - The API to fetch data from
+	// @function axios - Send GET request to fetch data from the API
+	// @param {string} "http://202.61.225.240:3000/beewhoyouwant" - The API URL to fetch the data from
+	// @function setUser - Where the data fetched from the API will be stored
 	// @returns {Promise object} res.data - The data fetched from the API
 	// @returns {boolean} false - The value of setLoading is set again to false once the Promise has been fulfilled
 
