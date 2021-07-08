18be6b4bf0f13335b1414e110f838bf1e43b4db5
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-07-09 01:07:58 +0200

    CSSed bio component

--- src/components/UserProfile/EditProfile/styles/TextBio.js
+++ src/components/UserProfile/EditProfile/styles/TextBio.js
@@ -1,5 +1,6 @@
 import { TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
+import autoprefixer from "autoprefixer";
 
 const TextBio = withStyles(() => ({
 	root: {
@@ -7,8 +8,11 @@ const TextBio = withStyles(() => ({
 		marginBottom: "2.5%",
 		borderRadius: "4px",
 		boxShadow: "0 8px 6px -6px rgba(0, 0, 0, 0.1)",
-		width: "300px",
+		width: "343px",
 		height: "150px",
+		"& .MuiOutlinedInput-notchedOutline": {
+			borderColor: "transparent",
+		},
 	},
 }))(TextField);
 
