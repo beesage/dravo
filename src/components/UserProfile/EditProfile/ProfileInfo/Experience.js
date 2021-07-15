2365da60bf1dd966b94f9d02c39d94df7512325c
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-07-15 22:29:20 +0200

    Refactor edit tablet organization

--- src/components/UserProfile/EditProfile/ProfileInfo/Experience.js
+++ src/components/UserProfile/EditProfile/ProfileInfo/Experience.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 import APIContext from "../../../../Context/APIContext";
 import UpdateInfo from "../Functions/UpdateInfo";
@@ -8,6 +9,10 @@ import LoadingPage from "../../../Spinner/LoadingPage";
 import { Container } from "@material-ui/core";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
 
 import "../styles/EditProfile.css";
 import useStylesEdit from "../styles/EditStyle";
@@ -61,37 +66,73 @@ export default function Experience() {
 				<LoadingPage />
 			)}
 			{user.length > 0 ? (
-				<div className="u-edit-container-tablet">
-					<Container
-						component="main"
-						maxWidth={false}
-						className={classesEdit.containerTablet}
-					>
-						<form className={classesEdit.root}>
-							<p className="edit-caption">Experience</p>
-							<Input
-								name="experience"
-								type="number"
-								id="formExperience"
-								onChange={handleChangeParse}
-								value={edited.experience}
-								className={classesEdit.textField}
-							/>
-							{err && (
-								<p className="err-message">{err.validationErrors[0].message}</p>
-							)}
-							<Button
-								value="Update"
-								text="Update"
-								style={{ fontSize: "1em" }}
-								onClick={updateExperience}
-								className={classesEdit.buttonEdit}
-							>
-								Update
-							</Button>
-							{res && <p className="err-message">{res}</p>}
-						</form>
-					</Container>
+				<div className="tablet-editprofile">
+					<div className="row">
+						<div className="column left">
+							<List>
+								<Link to="/settings/personal-info">
+									<ListItem>
+										<ListItemText>
+											<p className="menu-item-tablet">Personal Information</p>
+										</ListItemText>
+									</ListItem>
+								</Link>
+								<Divider />
+								<Link to="/settings/personal-info/password">
+									<ListItem>
+										<ListItemText>
+											<p className="menu-item-tablet">Password</p>
+										</ListItemText>
+									</ListItem>
+								</Link>
+								<Divider />
+								<Link to="/settings/profile-info">
+									<ListItem>
+										<ListItemText>
+											<p className="menu-item-tablet">Profile Information</p>
+										</ListItemText>
+									</ListItem>
+								</Link>
+								<Divider />
+							</List>
+						</div>
+						<div className="column right">
+							<div className="u-edit-container-tablet">
+								<Container
+									component="main"
+									maxWidth={false}
+									className={classesEdit.containerTablet}
+								>
+									<form className={classesEdit.root}>
+										<p className="edit-caption">Experience</p>
+										<Input
+											name="experience"
+											type="number"
+											id="formExperience"
+											onChange={handleChangeParse}
+											value={edited.experience}
+											className={classesEdit.textField}
+										/>
+										{err && (
+											<p className="err-message">
+												{err.validationErrors[0].message}
+											</p>
+										)}
+										<Button
+											value="Update"
+											text="Update"
+											style={{ fontSize: "1em" }}
+											onClick={updateExperience}
+											className={classesEdit.buttonEdit}
+										>
+											Update
+										</Button>
+										{res && <p className="err-message">{res}</p>}
+									</form>
+								</Container>
+							</div>
+						</div>
+					</div>
 				</div>
 			) : (
 				<LoadingPage />
