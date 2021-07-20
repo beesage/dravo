abd4ea1a101d34d89a9b8a85da7957ea71f62360
lukaskreibig <lukatze@posteo.de>
2021-07-20 10:23:13 +0200

    change grid to 10 rows, change wrong grid code, wrap login and sign up inside new grid

--- src/components/UserForm/styles/StyleUserForm.js
+++ src/components/UserForm/styles/StyleUserForm.js
@@ -1,8 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  root:{
-    fontFamily: "Krub",    
+  root: {
+    fontFamily: "Krub",
   },
   container: {
     backgroundColor: "#F7F6E7",
@@ -16,51 +16,51 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: "#314E52",
     fontFamily: "Krub",
-  },  
+  },
   logo: {
     marginTop: "10%",
     marginBottom: "10%",
-  },  
-  textField: {    
+  },
+  textField: {
     background: "#F2A154",
     marginBottom: "2.5%",
     borderRadius: "4px",
     boxShadow: "0 8px 6px -6px rgba(0, 0, 0, 0.1)",
-  }, 
+  },
   forgotPassword: {
     color: "#314E52",
     textDecoration: "underline",
     lineHeight: 5.3,
-    fontSize: "1.1em",    
+    fontSize: "1.1em",
     fontFamily: "Krub",
     fontWeight: "500",
   },
   submit: {
     backgroundColor: "#314E52",
-    color: "#F2A154",    
+    color: "#F2A154",
     marginBottom: "2%",
-    fontWeight: "800",    
+    fontWeight: "800",
     textTransform: "none",
     fontSize: "1.5em",
     cursor: "pointer",
     transitionDuration: "0.4s",
-    boxShadow: "0 8px 6px -6px rgba(0, 0, 0, 0.5)",  
-    '&:hover': {     
-      backgroundColor: "#5A8288",      
-  },
+    boxShadow: "0 8px 6px -6px rgba(0, 0, 0, 0.5)",
+    "&:hover": {
+      backgroundColor: "#5A8288",
+    },
   },
   link: {
     color: "#F2A154",
     textDecoration: "underline",
   },
   greenColor: {
-    color: "#314E52",   
+    color: "#314E52",
   },
   orangeColor: {
     color: "#F2A154",
     fontWeight: "500",
   },
- redColor: {
+  redColor: {
     color: "red",
   },
   checkbox: {
@@ -88,8 +88,8 @@ const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.up("md")]: {
       marginTop: "20%",
-    },    
-  },  
+    },
+  },
 }));
 
 export default useStyles;
