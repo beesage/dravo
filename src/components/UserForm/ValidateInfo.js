e0727fda03a15e94d4d569a291042b18bbfb1d04
PamelaFeijo <pamelaffeijo@gmail.com>
2021-07-20 16:19:25 +0200

    Fix validation on front end

--- src/components/UserForm/ValidateInfo.js
+++ src/components/UserForm/ValidateInfo.js
@@ -20,7 +20,7 @@ export default function validateInfo(values) {
     }
   
     if (!values.confirmPassword) {
-      errors.confirmPassword = 'Password is required';
+      errors.confirmPassword = 'Confirm password';
     } else if (values.confirmPassword !== values.password) {
       errors.confirmPassword = 'Passwords do not match';
     }
