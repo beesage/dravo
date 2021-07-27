5423607d336605ddf39f130afb186632f8740551
PamelaFeijo <pamelaffeijo@gmail.com>
2021-07-27 20:46:15 +0200

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
+import { Typography } from "@material-ui/core";
 import logo from "../../assets/logo-mobile.png";
 
 import "./styles/Footer.css";
+
+function Copyright() {
+	return (
+	  <Typography variant="body2" color="white" align="center">
+		{"Hospitalu 7-13, Riga, LV-1013"} <br/> 
+		{"Dravo © "}{new Date().getFullYear()}{". All rights reserved."}
+	  </Typography>
+	);
+  }
 
 export default function Footer() {
 	const [isLogin, setIsLogin] = useState(false);
@@ -30,8 +39,11 @@ export default function Footer() {
 	return (
 		<>
 			<footer className={!isLogin || !isSignUp ? "show" : "hidden"}>
-				<img src={logo} className="footer-img" />
-				<p style={{ color: "black" }}>DRAVO</p>
+				<div className="footer-container">
+					<img src={logo} className="footer-img" />
+					<p style={{ color: "#314E52", fontWeight: 800 }}>DRAVO</p>
+					<Copyright />
+				</div>
 			</footer>
 		</>
 	);
