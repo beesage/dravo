c6d8c5ff098a4aac02aa05a817c85bcadae1baa1
lukaskreibig <lukatze@posteo.de>
2021-07-28 20:49:24 +0200

    redid footer completely

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,49 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Typography } from "@material-ui/core";
-import logo from "../../assets/logo-mobile.png";
 
 import "./styles/Footer.css";
 
-function Copyright() {
-	return (
-	  <Typography variant="body2" color="white" align="center">
-		{"Hospitalu 7-13, Riga, LV-1013"} <br/> 
-		{"Dravo © "}{new Date().getFullYear()}{". All rights reserved."}
-	  </Typography>
-	);
-  }
-
 export default function Footer() {
-	const [isLogin, setIsLogin] = useState(false);
-	const [isSignUp, setIsSignUp] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
 
-	let location = useLocation();
+  let location = useLocation();
 
-	useEffect(() => {
-		if (location.pathname == "/" || location.pathname == "/signup") {
-			setIsSignUp(true);
-			setIsLogin(true);
-		} else if (location.pathname == "/leaderboard") {
-			setIsSignUp(false);
-			setIsLogin(false);
-		} else if (location.pathname == "/settings") {
-			setIsSignUp(true);
-			setIsLogin(true);
-		} else if (location.pathname == "/profile") {
-			setIsSignUp(false);
-			setIsLogin(false);
-		}
-	}, [location.pathname]);
+  useEffect(() => {
+    if (location.pathname == "/" || location.pathname == "/signup") {
+      setIsSignUp(true);
+      setIsLogin(true);
+    } else if (location.pathname == "/leaderboard") {
+      setIsSignUp(false);
+      setIsLogin(false);
+    } else if (location.pathname == "/settings") {
+      setIsSignUp(true);
+      setIsLogin(true);
+    } else if (location.pathname == "/profile") {
+      setIsSignUp(false);
+      setIsLogin(false);
+    }
+  }, [location.pathname]);
 
-	return (
-		<>
-			<footer className={!isLogin || !isSignUp ? "show" : "hidden"}>
-				<div className="footer-container">					
-					<p style={{ color: "#314E52", fontWeight: 800, paddingTop: "3%" }}>DRAVO</p>
-					<Copyright />
-				</div>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <footer className={!isLogin || !isSignUp ? "show" : "hidden"}>
+        <div className="footer-container">
+          <br /> Dravo © {new Date().getFullYear()} <br />
+          Hospitalu 7-13, Riga, LV-1013 <br /> All rights reserved.
+        </div>
+      </footer>
+    </>
+  );
 }
